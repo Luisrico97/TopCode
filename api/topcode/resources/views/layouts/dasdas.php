a1ca01202e6ae14c3e8f449272fd631886f029ad
Luisrico97 <luis.1997.ricoj@gmail.com>
2024-01-19 22:57:58 -0700

    Se realizo trabajo en el landing page. Agregamos un navbar con multiples botones. Tambien agregamos un fondo gradient. Le agregamos un carrucel para imagenes con informacion de nuestra pagina

---
+++ api/topcode/resources/views/layouts/dasdas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <h1></h1>
+</nav>
+</body>
+</html>
+
+
+
+<div>
+                <a href="{{ url('/') }}" class="text-lg font-semibold">Logo</a>
+            </div>
+            <div>
+              /*  @if (Route::has('login'))
+                    <a href="{{ route('login') }}" class="text-sm">Log in</a>
+                @endif
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="text-sm">Register</a>
+                @endif
+            </div>
